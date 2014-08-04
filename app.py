f492fbdb1af767b3197d95cef711ecdcc1196bf3
Mark Ronquillo <markronquillo23@gmail.com>
2014-08-04 10:57:58 +0800

    added flask script extension

--- app.py
+++ app.py
@@ -1,2 +1,39 @@
 from flask import Flask
+from flask import make_response, redirect, abort
+from flask.ext.script import Manager
+
 app = Flask(__name__)
+manager = Manager(app)
+
+
+@app.route('/')
+def index():
+  return '<h1> Hello World </h1>'
+
+@app.route('/user/<name>')
+def user(name):
+  return '<h1>Hello, %s!</h1>' % name
+
+@app.route('/mresponse')
+def mresponse():
+  response = make_response('<h1> This document carries a cookie!</h1>')
+  response.set_cookie('answer', '42')
+  return response
+
+@app.route('/redirect')
+def redir():
+  return redirect('http://www.google.com')
+
+@app.route('/user_id/<id>')
+def get_id(id):
+  user = 3
+  if user != id:
+    abort(404)
+  return '<h1> Hello, %s</h1>' % id
+
+if __name__ == '__main__':
+  # app.run(debug=True)
+  manager.run()
+
+
+
